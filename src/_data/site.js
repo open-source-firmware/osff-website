1695cc9cf731ba32069d5d917c51b54d9eb789eb
Christian Walter <christian.walter@9elements.com>
2025-04-21 21:31:44 +0200

    feat: update metadata
    Signed-off-by: Christian Walter <christian.walter@9elements.com>

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,10 +1,10 @@
-const productionUrl = "https://9elements.com";
+const productionUrl = "https://osfw.foundation";
 const developmentUrl = "http://localhost:8080";
 
 export default {
-  name: "11ty Boilerplate",
-  authorName: "9elements",
+  name: "Open-Source Firmware Foundation",
+  authorName: "Open-Source Firmware Foundation",
   url: process.env.NODE_ENV === "production" ? productionUrl : developmentUrl,
-  authorEmail: "contact@9elements.com",
-  metaDesc: "A simple boilerplate for 11ty",
+  authorEmail: "contact@osfw.foundation",
+  metaDesc: "Open-Source Firmware Foundation is a non-profit organization dedicated to the development and promotion of open-source firmware.",
 };
